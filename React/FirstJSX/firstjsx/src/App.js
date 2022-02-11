f19a73a4ae4418ca99e9a6a0c599e14dba16468d
Ahmed W <ahmed.wehelie@gmail.com>
2022-02-11 13:57:07 -0500

    FirstJSX

---
+++ React/FirstJSX/firstjsx/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+      <>
+        <h1 className="my-class">Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </>
+    
+  );
+}
+
+export default App;
