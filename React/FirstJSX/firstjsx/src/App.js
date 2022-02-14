9288192c6ae9e6ba407281e5b6976d3a6fa3a4f3
Ahmed W <ahmed.wehelie@gmail.com>
2022-02-14 14:41:05 -0500

    Prop it up

--- React/FirstJSX/firstjsx/src/App.js
+++ React/FirstJSX/firstjsx/src/App.js
@@ -1,19 +1,17 @@
 
 import './App.css';
 import React from 'react';
+import PersonCard from './components/PersonCard'
 
 function App() {
   return (
-      <>
-        <h1 className="my-class">Hello Dojo!</h1>
-        <h2>Things I need to do:</h2>
-        <ul>
-          <li>Learn React</li>
-          <li>Climb Mt. Everest</li>
-          <li>Run a marathon</li>
-          <li>Feed the dogs</li>
-        </ul>
-      </>
+     <div>
+       <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor ={"Black"}/> 
+       <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor ={"Brown"}/> 
+       <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor ={"Brown"}/> 
+       <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor ={"Brown"}/> 
+      
+     </div>
     
   );
 }
