a759f081eb13130e3cbe78474093c6341b4e016d
Ahmed W <ahmed.wehelie@gmail.com>
2022-03-25 13:21:31 -0400

    Putting it Together

--- React/PuttingItTogether/puttingtogether/src/components/Person.js
+++ React/PuttingItTogether/puttingtogether/src/components/Person.js
@@ -1,22 +1,34 @@
-import React, {useState} from "react";
+import React, {Component} from "react";
 
-const Person = (props) =>{
+class Person extends Component {
 
-    const{firstName, lastName, hairColor, age} = props
-    const [currentAge, setCurrentAge] = useState(age)
+    constructor(props){
+        super(props)
 
-    const increaseAge = () =>{
-        setCurrentAge(currentAge+1)
+        this.state = {
+            age: this.props.age
+        }
     }
-
+    
+    // const{firstName, lastName, hairColor, age} = props
+    // const [currentAge, setCurrentAge] = useState(age)
+    
+    // increaseAge () {
+    //     this.setState({personAge:this.state.personAge +1})
+    // }
+    
+    render(){
+        
+        const {firstName, lastName, hairColor} = this.props
+        
     return(
         <div>
             <p>{lastName}, {firstName}</p>
-            <p>Age: {currentAge}</p>
+            <p>Age: {this.state.age}</p>
             <p>Hair Color: {hairColor}</p>
-            <button onClick={increaseAge}>Birthday Button</button>
+            <button onClick={()=> this.setState({age:this.state.age +1})}>Birthday Button</button>
         </div>
-    )
+    )}
 }
 
 export default Person
