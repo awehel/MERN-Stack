378b45229ef65aa184f04e57413165b2c9002674
Ahmed W <ahmed.wehelie@gmail.com>
2022-03-25 10:07:25 -0400

    Chaining

--- OOP/Test/User.js
+++ OOP/Test/User.js
@@ -8,14 +8,17 @@ class User {
 
     makeDeposit(amount) {
         this.accountBalance += amount;
+        return this
     }
 
     makeWithdrawal(amount){
         this.accountBalance -=amount;
+        return this
     }
 
     displayBalance(){
         console.log(this.name + " " + this.accountBalance)
+        return this
     }
 }
 
@@ -23,21 +26,11 @@ const harry = new User("Harry Potter", "hpotter@hogwarts.com")
 const hermoine = new User("Hermoine Granger", "hgranger@hogwarts.com")
 const ron = new User("Ron Weasley", "rweasley@hogwarts.com")
 
-harry.makeDeposit(100)
-harry.makeDeposit(50)
-harry.makeDeposit(150)
-harry.makeWithdrawal(200)
-harry.displayBalance()
+harry.makeDeposit(100).makeDeposit(50).makeDeposit(150).makeWithdrawal(200).displayBalance()
 
+hermoine.makeDeposit(200).makeDeposit(100).makeWithdrawal(25).makeWithdrawal(50).displayBalance()
 
-hermoine.makeDeposit(200)
-hermoine.makeDeposit(100)
-hermoine.makeWithdrawal(25)
-hermoine.makeWithdrawal(50)
-hermoine.displayBalance()
-
-ron.makeDeposit(200)
-ron.makeWithdrawal(100)
+ron.makeDeposit(200).makeWithdrawal(100)
 ron.makeWithdrawal(125)
 ron.makeWithdrawal(75)
 ron.displayBalance()
